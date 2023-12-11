46c95042b038ae54461009d91ebea21d8dc72a7a
seto10987 <62239408+seto10987@users.noreply.github.com>
2023-12-11 16:01:11 -0600

    Add files via upload

---
+++ scripts/locations.lua
@@ -0,0 +1,46 @@
+                    --beanbean area
+Tracker:AddLocations("locations/beanbean_area/beanbean_castle.json")
+Tracker:AddLocations("locations/beanbean_area/castle_sewers.json")
+Tracker:AddLocations("locations/beanbean_area/beanbean_town.json")
+                    --bowsers castle
+Tracker:AddLocations("locations/bowsers_castle/bowsers_castle.json")
+Tracker:AddLocations("locations/bowsers_castle/iggy_morton_lemmy.json")
+Tracker:AddLocations("locations/bowsers_castle/ludwig_roy.json")
+Tracker:AddLocations("locations/bowsers_castle/wendy_larry.json")
+                    --chucklehuck
+Tracker:AddLocations("locations/chucklehuck/chucklehuck.json")
+Tracker:AddLocations("locations/chucklehuck/chuckolator.json")
+Tracker:AddLocations("locations/chucklehuck/chucklehuck_woods.json")
+Tracker:AddLocations("locations/chucklehuck/deep_chucklehuck.json")
+                    --hoohoo
+Tracker:AddLocations("locations/hoohoo/hoohoo_mountain.json")
+Tracker:AddLocations("locations/hoohoo/hoohoo_mountainbase.json")
+Tracker:AddLocations("locations/hoohoo/hoohoo_village.json")
+Tracker:AddLocations("locations/hoohoo/stardust_fields.json")
+                    --hooniversity
+Tracker:AddLocations("locations/hooniversity/woohoo_hooniversity.json")
+Tracker:AddLocations("locations/hooniversity/woohoo_hooniversity_back.json")
+Tracker:AddLocations("locations/hooniversity/woohoo_hooniversity_hallways.json")
+                    --jokes end
+Tracker:AddLocations("locations/jokes_end/jokes_end_1.json")
+Tracker:AddLocations("locations/jokes_end/jokes_end_2.json")
+Tracker:AddLocations("locations/jokes_end/jokes_end_3.json")
+                    --ocean
+Tracker:AddLocations("locations/ocean/oho_oasis.json")
+Tracker:AddLocations("locations/ocean/oho_seabed.json")
+Tracker:AddLocations("locations/ocean/gwarhar_lagoon_north.json")
+Tracker:AddLocations("locations/ocean/gwarhar_lagoon_south.json")
+Tracker:AddLocations("locations/ocean/oho_ocean_north.json")
+Tracker:AddLocations("locations/ocean/oho_ocean_south.json")
+                    --outskirts
+Tracker:AddLocations("locations/outskirts/outskirts_north.json")
+Tracker:AddLocations("locations/outskirts/outskirts_northeast.json")
+Tracker:AddLocations("locations/outskirts/outskirts_northwest.json")
+Tracker:AddLocations("locations/outskirts/outskirts_south.json")
+Tracker:AddLocations("locations/outskirts/outskirts_southeast.json")
+Tracker:AddLocations("locations/outskirts/outskirts_southwest.json")
+                    --teehee
+Tracker:AddLocations("locations/teehee/ss_chuckola.json")
+Tracker:AddLocations("locations/teehee/teehee_valley.json")
+Tracker:AddLocations("locations/teehee/little_fungitown.json")
+Tracker:AddLocations("locations/teehee/guffawha_ruins.json")
---
+++ scripts/logic.lua
@@ -0,0 +1,81 @@
+function has(item, amount)
+  local count = Tracker:ProviderCountForCode(item)
+  amount = tonumber(amount)
+  if not amount then
+    return count > 0
+  else
+    return count == amount
+  end
+end
+function CANDIG()
+    return
+    has ("hammerbrosl") and has ("hammer1")
+end
+function CANMINI()
+    return
+    has ("hammerbrosm") and has ("hammer1")
+end
+function CANGOBLETS()
+    return
+    has ("hammer1") and has ("hammerbrosm") and has ("hammerbrosl")
+end
+function CANDASH()
+    return
+    has ("handmario") and has ("handbrosm")
+end
+function CANCRASH()
+    return
+    has ("handluigi") and has ("handbrosl")
+end
+function CANPEARL()
+    return
+    has ("$CANDASH") and has ("$CANCRASH")
+end
+function FRUITS()
+    return
+    has ("keybrooch") and has ("purplefruit") and has ("whitefruit") and has ("redfruit")
+end
+function EGGS()
+    return
+    has ("keyblueegg") and has ("keyredegg") and has ("keyyellowegg") and has ("keygreenegg") and has ("keyorangeegg") and has ("keypurpleegg") and has ("keyazureegg")
+end
+function BEANFRUITS()
+    return
+    has ("keybeanfruit1") or has ("keybeanfruit2") or has ("keybeanfruit3") or has ("keybeanfruit4") or has ("keybeanfruit5") or has ("keybeanfruit6") or has ("keybeanfruit7")
+end
+function ALLBEANFRUITS()
+    return
+    has ("keybeanfruit1") and has ("keybeanfruit2") and has ("keybeanfruit3") and has ("keybeanfruit4") and has ("keybeanfruit5") and has ("keybeanfruit6") and has ("keybeanfruit7")
+end
+function SSACCESS()
+    return
+    has ("$CANDASH") or has ("hammer2")
+end
+function SSSINK()
+    return
+    has ("$SSACCESS") and has ("handmario") and has ("keymember")
+end
+function OCEANACCESS1()
+    return
+    has ("popple2") and has ("hammer3")
+end
+function OCEANACCESS2()
+    return
+    has ("$SSSINK") and has ("hammer3")
+end
+function OCEAN()
+    return
+    has ("$OCEANACCESS1") or has ("$OCEANACCESS2")
+end
+function ACCESSFUNGITOWN()
+    return
+    has ("queenbean") and has ("chuckolator") and has ("mompiranha")
+end
+function ACCESSJOKES()
+    return
+    has ("$OCEAN") and has ("hammer3")
+end
+function BOWSERACCESS()
+    return
+    has ("chuckolator") and has ("popple4")
+end
